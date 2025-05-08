27715fc1a642c0993f451edf9ccb54aa4551cbca
高尾　光佳 <m-takao@tc-corp.co.jp>
2025-05-09 02:46:32 +0900

    vutifyを適用

---
+++ plugins/vuetify.ts
@@ -0,0 +1,33 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+import * as components from 'vuetify/components'
+
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    theme: {
+      defaultTheme: 'myCustomLightTheme',
+      themes: {
+        myCustomLightTheme,
+      },
+    },
+    components
+  })
+  app.vueApp.use(vuetify)
+})
